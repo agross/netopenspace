b2201751e0a843e706a5e5cb059abc99a2d82c21
agross <agross@82e40cb3-3bb9-4a4d-9f4b-96774987b71d>
2009-04-16 11:13:26 +0000

    Adding a dialog stating that IE 6 and lower is outdated, effectively disabling the page

--- source/app/NOS.Wiki/public/HtmlHead.cs
+++ source/app/NOS.Wiki/public/HtmlHead.cs
@@ -4,3 +4,7 @@
 <!--[if lt IE 7]>
 <link rel="stylesheet" href="Themes/NOS/hacks/lt IE 7.css" type="text/css" />
 <![endif]-->
+<!--[if lte IE 6]>
+<script src="Themes/NOS/ie6-upgrade-warning/warning.js"></script>
+<script>window.onload = function() { e("Themes/NOS/ie6-upgrade-warning/") }</script>
+<![endif]-->
