8690ed466bb9a64694bc2903b938e19131d90d74
agross <agross@82e40cb3-3bb9-4a4d-9f4b-96774987b71d>
2009-04-16 16:23:44 +0000

    Adding a preliminary version of the sidebar (past local & other open space locations)

---
+++ source/app/NOS.Wiki/Themes/NOS/js-6-other-open-spaces.js
@@ -0,0 +1,65 @@
+$(document).ready(function()
+{
+	String.prototype.startsWith = function(instance, ignoreCase) {
+		if (ignoreCase == false)
+		{
+			return (instance == this.substring(0, instance.length));
+		}
+
+		return (instance.toLowerCase() == this.substring(0, instance.length).toLowerCase());
+	} 
+
+	$.getJSON('http://netopenspace.de/all-net-open-spaces.ashx?jsoncallback=?',
+			function (json) {
+				var local = $.grep(json, isLocalEvent)[0];
+				var remotes = $($.grep(json, isLocalEvent, true));
+				
+				if (local && local.PastEvents)
+				{
+					var list = buildList(local.PastEvents,
+						function() { return "past-spaces"; },
+						function() { return "Vergangene .NET Open Spaces in " + local.City; },
+						function(item) { return item.Year; },
+						function(item) { return ".NET Open Space " + item.Year + " in " + local.City; },
+						function(item) { return item.URL; })
+						
+					list.insertAfter("#HeaderDiv");
+				}
+				
+				if (remotes)
+				{
+					var list = buildList(remotes,
+						function() { return "remote-spaces"; },
+						function() { return "Andere .NET Open Spaces"; },
+						function(item) { return item.City; },
+						function(item) { return ".NET Open Space in " + item.City; },
+						function(item) { return item.URL; })
+											
+					list.insertAfter("#HeaderDiv");
+				}
+			}
+		);
+});
+
+function isLocalEvent(item)
+{
+	return document.URL.startsWith(item.URL, true);
+}
+
+function buildList(items, listClass, listHeader, itemText, itemTitle, itemUrl)
+{
+	var ul = $("<ul>")
+					
+	var list = $("<div>")
+		.attr("class", listClass())
+		.append("<p>")
+		.text(listHeader())
+		.append(ul);
+						
+	$(items).each(function(i, item) {
+		var a = $("<a>").text(itemText(item)).attr("href", itemUrl(item)).attr("title", itemTitle(item));
+		$('<li>').append(a).appendTo(ul); 
+	});
+	
+	return list;
+}
