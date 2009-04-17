81eeafb80e9724cc8da204a21256fa515b66659a
agross <agross@82e40cb3-3bb9-4a4d-9f4b-96774987b71d>
2009-04-17 15:37:10 +0000

    Moving some JS functions to a private scope

--- source/app/NOS.Wiki/Themes/NOS/js-6-other-open-spaces.js
+++ source/app/NOS.Wiki/Themes/NOS/js-6-other-open-spaces.js
@@ -1,30 +1,4 @@
-﻿function isLocalEvent(item)
-{
-	return document.URL.startsWith(item.URL, true);
-}
-
-function buildList(items, listClass, listHeader, itemText, itemTitle, itemUrl)
-{
-	var ul = $("<ul>");
-					
-	var list = $("<div>")
-		.attr("class", listClass())
-		.append($("<div>")
-			.attr("class", "submenu")
-			.append($("<p>").text(listHeader()))
-			.append(ul));
-						
-	$(items).each(function(i, item) {
-		var a = $("<a>").text(itemText(item)).attr("href", itemUrl(item)).attr("title", itemTitle(item));
-		$('<li>').append(a).appendTo(ul);
-	});
-
-	list.droppy('> div');
-	
-	return list;
-}
-
-$(document).ready(function()
+﻿$(document).ready(function()
 {
 	String.prototype.startsWith = function(instance, ignoreCase)
 	{
@@ -36,11 +10,38 @@ $(document).ready(function()
 		return (instance.toLowerCase() == this.substring(0, instance.length).toLowerCase());
 	};
 
+	var localEventFilter = function(item)
+	{
+		return document.URL.startsWith(item.URL, true);
+	}
+
+	var buildList = function(items, listClass, listHeader, itemText, itemTitle, itemUrl)
+	{
+		var ul = $("<ul>");
+
+		var list = $("<div>")
+		.attr("class", listClass())
+		.append($("<div>")
+			.attr("class", "submenu")
+			.append($("<p>").text(listHeader()))
+			.append(ul));
+
+		$(items).each(function(i, item)
+		{
+			var a = $("<a>").text(itemText(item)).attr("href", itemUrl(item)).attr("title", itemTitle(item));
+			$('<li>').append(a).appendTo(ul);
+		});
+
+		list.droppy('> div');
+
+		return list;
+	}
+
 	$.getJSON('http://netopenspace.de/all-net-open-spaces.json?json=?',
 			function(json)
 			{
-				var local = $.grep(json, isLocalEvent)[0];
-				var remotes = $($.grep(json, isLocalEvent, true));
+				var local = $.grep(json, localEventFilter)[0];
+				var remotes = $($.grep(json, localEventFilter, true));
 
 				if (local && local.PastEvents)
 				{
