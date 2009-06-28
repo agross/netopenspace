e2b0b67478d8c45e725dddd8a241964be6b563be
agross <agross@82e40cb3-3bb9-4a4d-9f4b-96774987b71d>
2009-06-28 15:49:03 +0000

    Beginning to fix a bug in the code copying functionality (we need a customized flash file to make this work)

--- source/app/NOS.Wiki/Themes/NOS/js-2-copy-code.js
+++ source/app/NOS.Wiki/Themes/NOS/js-2-copy-code.js
@@ -2,122 +2,99 @@
 {
 	copyCodeText: "Kopieren",
 	copyCodeTitle: "In die Zwischenablage kopieren",
+	rand: +new Date,
+	
 	init: function()
 	{
-		if (!document.getElementsByTagName || !document.createElement)
-		{
-			return;
-		}
-
-		var pres = document.getElementsByTagName("pre");
-		for (var i = 0; i < pres.length; i++)
-		{
-			var pre = pres[i];
-			if(copyCode.isItemPre(pre))
-			{
-				var copyLink = document.createElement("a");
-				copyLink.setAttribute("title", copyCode.copyCodeTitle);
-				if (BrowserDetect.browser === "Explorer")
-				{
-					copyLink.onclick = function() { copyCode.copyToClipboard(this); };
-				}
-				else
-				{
-					copyLink.setAttribute("onclick", "copyCode.copyToClipboard(this);");
-				}
-				copyLink.className = "copy-code";
-				copyLink.appendChild(document.createTextNode(copyCode.copyCodeText));
-
-				pre.parentNode.insertBefore(copyLink, pre);
-			}
-		}
-	},
-	isInContent: function(element)
-	{
-		if(element.parentNode && typeof(element.parentNode.id) !== "undefined")
-		{
-			if(element.parentNode.id.search(/PageContentDiv/i) !== -1)
-			{
-				return true;
-			}
-			else
-			{
-				return copyCode.isInContent(element.parentNode);
-			}
-		}
-		else
-		{
-			return false;
-		}
-	},
-	isItemPre: function(pre)
-	{
-		return pre.className.search(/no-copy/i) === -1 && // Exclude explicitly excluded pres.
-			copyCode.isInContent(pre);
+		var link = $("<a>")
+			.text(copyCode.copyCodeText)
+			.attr("title", copyCode.copyCodeTitle)
+			.addClass("copy-code")
+			.click(function() {
+				copyCode.copyToClipboard(this);
+			});
+			
+		link.insertBefore("#PageContentDiv pre");
 	},
 	copyToClipboard: function(link)
 	{
-		if (typeof(link) === "undefined")
+		if (!link)
 		{
 			return;
 		}
 
-		var text;
+		var pre = $(link).next()
+		var text = $.trim(pre.text());
 
 		if (window.clipboardData)
 		{
-			// IE.
-			text = copyCode.trim(link.nextSibling.innerText);
 			window.clipboardData.setData("Text", text);
+			copyCode.flash(pre);
+			return;
 		}
-		else
+		
+		if ($("div#flashcopier").length === 0)
 		{
-			text = copyCode.trim(link.nextSibling.textContent);
-
-			if (!document.getElementById("flashcopier"))
-			{
-				var flash = document.createElement("div");
-				flash.id = "flashcopier";
-				document.body.appendChild(flash);
-			}
-
-			document.getElementById("flashcopier").innerHTML = "";
-			var divinfo = '<embed src="Themes/NOS/clipboard.swf" FlashVars="clipboard=' + encodeURIComponent(text) + '" width="0" height="0" type="application/x-shockwave-flash"></embed>';
-			document.getElementById("flashcopier").innerHTML = divinfo;
+			$("<div>")
+				.attr("id", "flashcopier")
+				.css("display", "none")
+				.appendTo("body");
 		}
-	},
-	// Taken from http://blog.stevenlevithan.com/archives/faster-trim-javascript.
-	trim: function(str)
-	{
-		str = str.replace(/^\s\s*/, '');
-		var whitespace = /\s/;
-		var i = str.length;
-		while (whitespace.test(str.charAt(--i)));
 
-		return str.slice(0, i + 1);
-	},
-	addEvent: function(obj, eventTypes, callback)
-	{
-		var eventType;
-		for(var i = 0; i < eventTypes.length; i++)
-		{
-			eventType = eventTypes[i];
+		var id = copyCode.rand++;
+		
+		var flash = $("<embed>")
+				.attr("class", id)
+				.attr("src", "Themes/NOS/clipboard.swf")
+				.attr("flashvars", "highlighterId=" + id)
+				.attr("menu", false)
+				.attr("allowscriptaccess", "always")
+				.attr("wmode", "transparent")
+				.attr("width", 16)
+				.attr("height", 16)
+				.attr("type", "application/x-shockwave-flash");
+				
+		$("div#flashcopier")
+			.append($("<pre>")
+				.attr("id", id)
+				.text(text)
+			)
+			.append(flash);
 
-			if (obj.attachEvent)
-			{
-				obj["e" + eventType + callback] = callback;
-				obj[eventType + callback] = function()
-				{
-					obj["e" + eventType + callback](window.event);
-				};
-				obj.attachEvent("on" + eventType, obj[eventType + callback]);
-			}
-			else
-			{
-				obj.addEventListener(eventType, callback, false);
-			}
-		}
+		copyCode.flash(pre);
+	},
+	flash: function(element)
+	{
+		element.animate({ backgroundColor: "#E1DF9E" }, 100);
+		element.animate({ backgroundColor: "#F6F6F6" }, 400);
 	}
 };
 
-copyCode.addEvent(window, ["load"], copyCode.init);
+/* The below code extends the animate function so that it works with color animations */
+/* By John Resig */
+(function(jQuery){
+jQuery.each(['backgroundColor','borderBottomColor','borderLeftColor','borderRightColor','borderTopColor','color','outlineColor'],function(i,attr){jQuery.fx.step[attr]=function(fx){if(fx.state==0){fx.start=getColor(fx.elem,attr);fx.end=getRGB(fx.end)}fx.elem.style[attr]="rgb("+[Math.max(Math.min(parseInt((fx.pos*(fx.end[0]-fx.start[0]))+fx.start[0]),255),0),Math.max(Math.min(parseInt((fx.pos*(fx.end[1]-fx.start[1]))+fx.start[1]),255),0),Math.max(Math.min(parseInt((fx.pos*(fx.end[2]-fx.start[2]))+fx.start[2]),255),0)].join(",")+")"}});
+function getRGB(color){var result;if(color&&color.constructor==Array&&color.length==3)return color;if(result=/rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)/.exec(color)){return[parseInt(result[1]),parseInt(result[2]),parseInt(result[3])]}if(result=/rgb\(\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*\)/.exec(color)){return[parseFloat(result[1])*2.55,parseFloat(result[2])*2.55,parseFloat(result[3])*2.55]}if(result=/#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})/.exec(color)){return[parseInt(result[1],16),parseInt(result[2],16),parseInt(result[3],16)]}if(result=/#([a-fA-F0-9])([a-fA-F0-9])([a-fA-F0-9])/.exec(color)){return[parseInt(result[1]+result[1],16),parseInt(result[2]+result[2],16),parseInt(result[3]+result[3],16)]}return colors[jQuery.trim(color).toLowerCase()]}
+function getColor(elem,attr){var color;do{color=jQuery.curCSS(elem,attr);if(color!=''&&color!='transparent'||jQuery.nodeName(elem,"body")){break}attr="backgroundColor"}while(elem=elem.parentNode);return getRGB(color)};
+var colors={aqua:[0,255,255],azure:[240,255,255],beige:[245,245,220],black:[0,0,0],blue:[0,0,255],brown:[165,42,42],cyan:[0,255,255],darkblue:[0,0,139],darkcyan:[0,139,139],darkgrey:[169,169,169],darkgreen:[0,100,0],darkkhaki:[189,183,107],darkmagenta:[139,0,139],darkolivegreen:[85,107,47],darkorange:[255,140,0],darkorchid:[153,50,204],darkred:[139,0,0],darksalmon:[233,150,122],darkviolet:[148,0,211],fuchsia:[255,0,255],gold:[255,215,0],green:[0,128,0],indigo:[75,0,130],khaki:[240,230,140],lightblue:[173,216,230],lightcyan:[224,255,255],lightgreen:[144,238,144],lightgrey:[211,211,211],lightpink:[255,182,193],lightyellow:[255,255,224],lime:[0,255,0],magenta:[255,0,255],maroon:[128,0,0],navy:[0,0,128],olive:[128,128,0],orange:[255,165,0],pink:[255,192,203],purple:[128,0,128],violet:[128,0,128],red:[255,0,0],silver:[192,192,192],white:[255,255,255],yellow:[255,255,0]};
+})(jQuery);
+
+var SyntaxHighlighter =
+{
+	toolbar: {
+		executeCommand: function(sender, event, highlighterId, commandName, args)
+		{
+			if (args !== "get")
+			{
+				$("pre#" + highlighterId).remove();
+				$("embed." + highlighterId).remove();
+			}
+			return $("pre#" + highlighterId).text();$("<pre>")
+		}
+	}
+}
+
+$(document).ready(function()
+{
+	copyCode.init();
+});
