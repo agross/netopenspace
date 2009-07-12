2149fc1770c3452d4201e08010c9a4b080991581
agross <agross@82e40cb3-3bb9-4a4d-9f4b-96774987b71d>
2009-07-12 14:59:33 +0000

    Adding Twitter buzz

---
+++ source/app/NOS.Wiki/Themes/NOS/js-7-twitter-buzz.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function()
+{
+	$('<div>')
+		.addClass('buzz')
+		.addClass('no-print')
+		.append($('<div>').addClass('content')
+			.append($('<p>').text('Buzz'))
+			.append($('<div>')
+				.addClass('monitter')
+				.attr('title', '#netos2009 OR #nos_sued')))
+		.insertAfter("#HeaderDiv");
+});
