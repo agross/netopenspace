cda156250d19fd1390ab37409a159228f5bc350f
agross <agross@82e40cb3-3bb9-4a4d-9f4b-96774987b71d>
2009-08-12 23:13:42 +0000

    Increasing hard limit to 130

--- source/app/NOS.Wiki/public/Plugins/Config/NOS.Registration.AutoRegistrationPlugin.cs
+++ source/app/NOS.Wiki/public/Plugins/Config/NOS.Registration.AutoRegistrationPlugin.cs
@@ -4,5 +4,5 @@ ListEndPattern=^\s*<!-- hier davor eintragen, nachfolgenden Zeilenumbruch bitte 
 WaitingListEndPattern=^\s*<!-- hier davor am Ende eintragen, nachfolgenden Zeilenumbruch bitte nicht entfernen -->\s*$
 EntryPattern=^#.*$
 MaximumAttendees=100
-HardLimit=120
+HardLimit=130
 EntryTemplate=# $user.Data.Name#if($user.Data.Email), [$user.Data.Email|E-Mail]#end#if($user.Data.Blog), [$user.Data.Blog|Blog]#end#if($user.Data.Twitter), [http://twitter.com/$user.Data.Twitter/|Twitter]#end#if($user.Data.Xing), [http://xing.com/profile/$user.Data.Xing|XING]#end#if($user.Data.Picture), [$user.Data.Picture|Bild]#end#if($user.Data.Sponsoring > 0), $user.Data.FormattedSponsoring &euro;#end\n\n
