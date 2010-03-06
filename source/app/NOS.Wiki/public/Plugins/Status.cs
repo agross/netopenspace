03305d80e2dcfa51f1f76a7f4b588b1f3847872f
Alexander Groß <agross@therightstuff.de>
2010-03-06 17:16:46 +0100

    Updating Plugin Pack, enabling the Multilanguage Content plugin

---
+++ source/app/NOS.Wiki/public/Plugins/Status.cs
@@ -0,0 +1,4 @@
+ScrewTurn.Wiki.Plugins.PluginPack.UnfuddleTickets
+ScrewTurn.Wiki.Plugins.PluginPack.DownloadCounter
+ScrewTurn.Wiki.Plugins.PluginPack.Footnotes
+ScrewTurn.Wiki.Plugins.PluginPack.RssFeedDisplay
