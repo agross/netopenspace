2507d3b60464589f620c5fd0e195198b62283039
Alexander Groß <agross@therightstuff.de>
2010-03-07 14:48:36 +0100

    Fixing header and footer layout issues

--- source/app/NOS.Wiki/public/Footer.cs
+++ source/app/NOS.Wiki/public/Footer.cs
@@ -1,11 +1,14 @@
-﻿<div class="content">
-<div class="logos">
+﻿<div id="footer">
+	<div class="content">
+		<div class="logos">
 * [image|Dieses Wiki steht unter der Creative Commons-Lizenz|{UP}Layout/logo-cc-wiki.png|http://creativecommons.org/licenses/by-sa/3.0/deed.de]
 * [image|In Kooperation mit der .NET User Group Leipzig|{UP}Layout/logo-dnug-leipzig.gif|http://dotnet-leipzig.de/]
 * [image|Powered by ScrewTurn Wiki|{UP}Layout/logo-screwturn.gif|http://www.screwturn.eu/]
 * [Impressum]
-</div>Das Design dieser Seite wurde von [http://therightstuff.de/|Alexander Groß] erstellt.
-Einige Icons stammen von [http://www.famfamfam.com|FamFamFam]. Die Figuren im Logo sind von [http://pixelplastic.de/|Marcel Hoyer].
-Bildquelle: [http://www.aboutpixel.de|aboutpixel.de] / [http://www.aboutpixel.de/index.php4?toppage=imagedetails&image_id=44930|Weißgrün - das Zebragras] / &copy; [http://www.aboutpixel.de/index.php4?toppage=imagebank&subpage=member&user_id=52283|Thorwald Hoffmann].
+		</div>
+		Das Design dieser Seite wurde von [http://therightstuff.de/|Alexander Groß] erstellt.
+		Einige Icons stammen von [http://www.famfamfam.com|FamFamFam]. Die Figuren im Logo sind von [http://pixelplastic.de/|Marcel Hoyer].
+		Bildquelle: [http://www.aboutpixel.de|aboutpixel.de] / [http://www.aboutpixel.de/index.php4?toppage=imagedetails&image_id=44930|Weißgrün - das Zebragras] / &copy; [http://www.aboutpixel.de/index.php4?toppage=imagebank&subpage=member&user_id=52283|Thorwald Hoffmann].
+	</div>
+	<div class="clearfix"></div>
 </div>
-<div class="clearfix"></div>
