8fa8abb3cf1eb236ea9f6d50bcb3b2b96d32cdad
Alexander Groß <agross@therightstuff.de>
2010-09-05 19:11:56 +0200

    Fixing NOS-1, now linking all hash tags in tweets

--- source/app/NOS.Wiki/Themes/NOS/includes/a2 - monitter.js
+++ source/app/NOS.Wiki/Themes/NOS/includes/a2 - monitter.js
@@ -18,7 +18,7 @@
 		};
 
 		String.prototype.linktag = function () {
-			return this.replace(/[#]+[A-Za-z0-9-_]+/, function (t) {
+			return this.replace(/[#]+[A-Za-z0-9-_]+/g, function (t) {
 				var tag = t.replace("#", "%23")
 				return t.link("http://search.twitter.com/search?q=" + tag);
 			});
