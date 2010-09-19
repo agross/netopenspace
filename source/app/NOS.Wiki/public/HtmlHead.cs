f3334cf6059766487bb542a955c7edac35a120be
Alexander Groß <agross@therightstuff.de>
2010-09-19 20:04:20 +0200

    Fixing IE layout issues, yay

--- source/app/NOS.Wiki/public/HtmlHead.cs
+++ source/app/NOS.Wiki/public/HtmlHead.cs
@@ -1,10 +1,11 @@
 ﻿<!--[if IE]>
 <link rel="stylesheet" href="Themes/NOS/hacks/IE.css" type="text/css" />
 <![endif]-->
-<!--[if lt IE 7]>
-<link rel="stylesheet" href="Themes/NOS/hacks/lt IE 7.css" type="text/css" />
+<!--[if IE 8]>
+<link rel="stylesheet" href="Themes/NOS/hacks/IE 8.css" type="text/css" />
 <![endif]-->
 <!--[if lte IE 6]>
+<link rel="stylesheet" href="Themes/NOS/hacks/lte IE 6.css" type="text/css" />
 <script src="Themes/NOS/ie6/warning.js"></script>
-<script>window.onload = function() { e("Themes/NOS/ie6/") }</script>
+<script>window.onload = function() { e("Themes/NOS/ie6/"); }</script>
 <![endif]-->
