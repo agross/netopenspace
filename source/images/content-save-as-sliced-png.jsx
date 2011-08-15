762800c714fae7d7a31df2d89f8b4bd2cc3959e1
Alexander Groß <agross@therightstuff.de>
2011-08-15 21:50:52 +0200

    Updating header logo to 2011

--- source/images/content-save-as-sliced-png.jsx
+++ source/images/content-save-as-sliced-png.jsx
@@ -6,13 +6,13 @@ var logoBounds = new Array(6, 6, 506, 140);
 var contentBounds = new Array(0, 143, 949, 144);
 var footerBounds = new Array(6, 144, 943, 150);
 
-cropAndSave(sourceDocument, headerBounds, "..\\app\\NOS.Wiki\\Themes\\NOS\\page\\images\\layout\\header-background.png", function(doc) {
+cropAndSave(sourceDocument, headerBounds, "..\\app\\NOS.Wiki\\Themes\\NOS\\images\\layout\\header-background.png", function(doc) {
 	doc.layerSets["Logo"].visible = false;
 });
 
-cropAndSave(sourceDocument, logoBounds, "..\\app\\NOS.Wiki\\Themes\\NOS\\page\\images\\layout\\header-logo.png");
-cropAndSave(sourceDocument, contentBounds, "..\\app\\NOS.Wiki\\Themes\\NOS\\page\\images\\layout\\content-border.png");
-cropAndSave(sourceDocument, footerBounds, "..\\app\\NOS.Wiki\\Themes\\NOS\\page\\images\\layout\\footer-background.png");
+cropAndSave(sourceDocument, logoBounds, "..\\app\\NOS.Wiki\\Themes\\NOS\\images\\layout\\header-logo.png");
+cropAndSave(sourceDocument, contentBounds, "..\\app\\NOS.Wiki\\Themes\\NOS\\images\\layout\\content-border.png");
+cropAndSave(sourceDocument, footerBounds, "..\\app\\NOS.Wiki\\Themes\\NOS\\images\\layout\\footer-background.png");
 
 function cropAndSave(sourceDocument, bounds, filename, processClone)
 {
