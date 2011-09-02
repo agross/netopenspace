36f329d881b1c07f786b9b590684a8b8b3da4ba9
Alexander Groß <agross@therightstuff.de>
2011-09-02 21:46:00 +0200

    Updating sidebar

--- source/app/NOS.Wiki/public/Sidebar.cs
+++ source/app/NOS.Wiki/public/Sidebar.cs
@@ -1,44 +1,14 @@
-<en-us>
-====Navigation <div class="rss"><a href="{MAINURL}RSS.aspx" title="Subscribe to the RSS Feed of {WIKITITLE}"><img src="{THEMEPATH}images/RSS-16x16.png" alt="RSS" title="Subscribe to the RSS Feed of {WIKITITLE}"/></a></div>====
-* '''[MainPage|Main Page]'''
-* [News]
-
-* [Category.aspx|Page Categories]
-* [AllPages.aspx|All Pages]
-
-* [Login.aspx|Login/Logout]
-* [Register|Register]
-* [Profile.aspx|Your Profile]
-* [Language.aspx|Language Selection]
-
-* [Edit.aspx|Create a new Page]
-* [Upload.aspx|File Management]
-* [Admin.aspx|Administration]
-
-====Search====
-{SEARCHBOX}{BR}
-<small>[Search.aspx?FullText=1|Advanced Search &raquo;]</small>
-
-====Partners====
-{t:Partnerlogos}
-====Sponsors====
-{t:Sponsorenlogos}
-('''Want to sponsor?''' [orga@netopenspace.de| Write us!])
-</en-us>
-
 <de-de>
 ====Navigation <div class="rss"><a href="{MAINURL}RSS.aspx" title="Den RSS-Feed von {WIKITITLE} abonnieren"><img src="{THEMEPATH}images/RSS-16x16.png" alt="RSS" title="Den RSS-Feed von {WIKITITLE} abonnieren"/></a></div>====
 * '''[MainPage|Hauptseite]'''
+* [Anmeldung|Anmeldung]
 * [News|Aktuelles]
+
+* [Login.aspx|Wiki An-/Abmeldung]
+* [Profile.aspx|Wiki Profil]
 
 * [AllPages.aspx|Alle Seiten]
 * [Category.aspx|Nach Kategorien]
-
-* [Login.aspx|An-/Abmelden]
-* [Register|Registrieren]
-* [Profile.aspx|Dein Profil]
-* [Language.aspx|Sprachauswahl]
-
 * [Edit.aspx|Neue Seite erstellen]
 * [Upload.aspx|Dateiverwaltung]
 * [Admin.aspx|Administration]
@@ -47,9 +17,10 @@
 {SEARCHBOX}{BR}
 <small>[Search.aspx?FullText=1|Erweiterte Suche &raquo;]</small>
 
+====Sponsoren====
+{t:Sponsorenlogos}
+('''[FAQ#Sponsoring_Unternehmen|Sponsor werden?]''')
+
 ====Partner====
 {t:Partnerlogos}
-====[Sponsoren]====
-{t:Sponsorenlogos}
-('''[FAQ#Sponsoring_Unternehmen|Wie wird gesponsert?]''')
 </de-de>
